ee1d15240d4f63819c920f81179643b3a47bd227
Alec Lorraine <alec@brighten.in>
2020-01-03 15:12:28 -0800

    new copy and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,46 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class="faq-container">
+      <div class="faq">
+        <h2>
+          Don't see your question here? You can always email us at{" "}
+          <a href="mailto:alexa@brighten.in">alexa@brighten.in</a> with any
+          questions! We love hearing from you :){" "}
+        </h2>
+
+        <h3> What is Timesuck? </h3>
+        <p>
+          Timesuck is an app that logs your screen time each day to help you
+          become more aware of how much time you spend on your phone.
+        </p>
+        <h3> Why do I need to allow location access?</h3>
+        <p>
+          Because of some iOS limitations, Timesuck needs access to location in
+          order to stay open in the background and log screen time. Your
+          location is stored locally on your phone, and is 100% private; we
+          don't ever see it.
+        </p>
+
+        <h3>How do I add friends?</h3>
+        <p>
+          Tap the add a friend button and search for their name or username.
+        </p>
+
+        <h3>My screentime is not accurate!</h3>
+        <p>
+          iOS does not allow access to their screen time api (we have no idea
+          why) so we have to use our own methods of tracking screen time, and
+          sometimes it messes up. We're working hard every day to make it more
+          accurate!
+        </p>
+
+        <p>
+          You're always welcome to text or email us through the app if you need
+          any help with anything at all!
+        </p>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
