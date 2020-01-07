f9d4edd3863c343338a3e17d0d425fb7e4aca033
Alec Lorraine <alec@brighten.in>
2020-01-06 17:32:12 -0800

    add anwers

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,12 +21,31 @@ const IndexPage = () => (
           Timesuck is an app that logs your screen time each day to help you
           become more aware of how much time you spend on your phone.
         </p>
+
+        <h3>
+          Does Timesuck have to be in the background to measure my screen time?
+        </h3>
+        <p>
+          Short answer: Yes. Timesuck must be running in the background to
+          measure when you’re using your phone. It does not have to be open and
+          active. You can press the home button and leave Timesuck in the
+          background, but you cannot completely force quit the app (swiping up
+          from the multitasking menu)
+        </p>
+
         <h3> Why do I need to allow location access?</h3>
         <p>
           Because of some iOS limitations, Timesuck needs access to location in
           order to stay open in the background and log screen time. Your
           location is stored locally on your phone, and is 100% private; we
           don't ever see it.
+        </p>
+
+        <h3>Does it drain battery?</h3>
+        <p>
+          Not really. Because it needs to be running the background, Timesuck
+          will use around 5% of your battery per day. You should barely notice
+          it.
         </p>
 
         <h3>How do I add friends?</h3>
